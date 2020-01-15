47fd8a57dbf9f0f0b92f5bc666d054e5d94828e1
Jaqueline <59242839+djjrosa@users.noreply.github.com>
2020-01-15 19:46:44 -0300

    Add files via upload

---
+++ maiores1.py
@@ -0,0 +1,23 @@
+﻿meuPrimo = 961
+maiorAnterior = 0
+
+def e_primo(k):
+  fator = 2
+  if k == 2:
+    return True
+  
+  while k % fator and fator <= k/2:
+    fator = fator + 1
+  if k % fator == 0:
+    return False
+  else:
+    return True
+
+def maior_primo(k):
+    i = 1
+    while e_primo(k - i) == False:
+      i = i + 1
+    
+    return k - i
+
+maiorAnterior = maior_primo(meuPrimo)
